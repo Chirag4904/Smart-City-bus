497df42e055265d4c2591c2b96ce6961483b9cf6
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-24 17:33:19 +0530

    first commit

---
+++ client/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	// console.log(window);
+	// import { onMount } from "svelte";
+
+	// // Instantiate the map using the vecor map with the
+	// // default style as the base layer:
+	// onMount(() => {
+	// 	var platform = new window.H.service.Platform({
+	// 		apikey: "6odJUv7kDQPaYtH2picifXqt27HJJ073KvFZHaEnrdY",
+	// 	});
+
+	// 	var defaultLayers = platform.createDefaultLayers();
+	// 	console.log(window.H);
+	// 	var map = new window.H.Map(
+	// 		document.getElementById("mapContainer"),
+	// 		defaultLayers.vector.normal.map
+	// 	);
+	// });
+</script>

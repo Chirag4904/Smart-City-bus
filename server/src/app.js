e80c51dab6a6008481222a1e2f3ab2d15c1876fc
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-26 02:47:06 +0530

    app update

--- server/src/app.js
+++ server/src/app.js
@@ -17,33 +17,31 @@ const AuthRoute = require("./routes/auth");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
+app.use(express.static(path.join(__dirname, "..", "dist")));
 
 app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-    console.error(err);
-    res.status(400).json({ error: true, error_desc: err }); // Bad request
-    return;
-  }
-  next();
+	if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+		console.error(err);
+		res.status(400).json({ error: true, error_desc: err }); // Bad request
+		return;
+	}
+	next();
 });
 
 app.use(
-  cors({
-    origin: "*",
-  })
+	cors({
+		origin: "*",
+	})
 );
 app.get("/", (req, res) => {
-  res.send("hello");
+	res.send("hello");
 });
 app.use("/routes", routesRouter);
 
 app.use("/auth", AuthRoute);
 
 app.get("/app", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "..", "..", "client", "dist", "index.html")
-  );
+	res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
 });
 
 module.exports = app;

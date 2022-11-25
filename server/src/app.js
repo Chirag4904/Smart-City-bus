60a8334b1f7db9a300810a131bd6c972a7589bf1
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-25 10:15:19 +0530

    backend added

---
+++ server/src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// const teachersRouter = require("./routes/teachers/teachers.router");
+// const subjectsRouter = require("./routes/subjects/subjects.router");
+
+/** setup logging */
+// source: https://stackoverflow.com/a/47074065
+
+/** =============== */
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+	if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+		console.error(err);
+		res.status(400).json({ error: true, error_desc: err }); // Bad request
+		return;
+	}
+	next();
+});
+
+app.use(
+	cors({
+		origin: "*",
+	})
+);
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+// app.use("/api/teachers", );
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
+// const { loadTeachersData } = require("./models/teachers.model");
+// const { loadsubjectsData } = require("./models/subjects.model");
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+	await mongoConnect(process.env.MONGODB_URL || "");
+
+	// await loadTeachersData();
+	// await loadsubjectsData();
+
+	server.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
+	});
+}
+
+startServer();

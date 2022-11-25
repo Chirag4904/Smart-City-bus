c4034e3a65ca98298d6b6604f0c831123aedaf7b
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-26 02:36:19 +0530

    build

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,14 @@
 <script>
-  import Login from "./Components/Login.svelte";
-  import Map from "./components/Map.svelte";
-  import Schedule from "./Components/Schedule.svelte";
-  import Wallet from "./Components/Wallet.svelte";
-  import Welcome from "./Components/Welcome.svelte";
-  import SignUp from "./Components/SignUp.svelte";
+	import Login from "./Components/Login.svelte";
+	import Map from "./components/Map.svelte";
+	import Schedule from "./Components/Schedule.svelte";
+	import Wallet from "./Components/Wallet.svelte";
+	import Welcome from "./Components/Welcome.svelte";
+	import SignUp from "./Components/SignUp.svelte";
+	import RestAPI from "./components/RestAPI.svelte";
 </script>
 
-<Wallet />
+<!-- <Schedule />
+<RestAPI /> -->
+<!-- <Wallet /> -->
+<Map />
